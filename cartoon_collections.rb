acb50e9ba2e576416c0182ee1b7bcb78b2fb99f9
Bahaeddine Bouslama <bahaeddineb@outlook.fr>
2018-08-10 07:57:17 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(calls)
-  return calls.detect { |call| call.length > 4}
+  return calls.any? { |call| call.length > 4}
 end
 
 def find_the_cheese
